1693409490f25ea47940559a1fcdad4b28f4f5df
Koushik Dutta <koushd@gmail.com>
2010-07-03 16:03:13 -0700

    board files

---
+++ AndroidBoard.mk
@@ -0,0 +1,16 @@
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel
+endif
+
+file := $(INSTALLED_KERNEL_TARGET)
+ALL_PREBUILT += $(file)
+$(file): $(TARGET_PREBUILT_KERNEL) | $(ACP)
+	$(transform-prebuilt-to-target)
+
+file := $(TARGET_ROOT_OUT)/init.buzz.rc
+ALL_PREBUILT += $(file)
+$(file) : $(LOCAL_PATH)/init.buzz.rc | $(ACP)
+	$(transform-prebuilt-to-target)
+
